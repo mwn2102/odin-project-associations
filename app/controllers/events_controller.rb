aa1dc5c8f6a78c435dbc3115b609c3d526acc95f
Michael Nelson <mwn2102@gmail.com>
2015-09-20 04:27:39 +0000

    completed setup, signin, and basic events

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,27 @@
+class EventsController < ApplicationController
+    def index
+        @events = Event.all
+    end
+    
+    def new
+        @event = Event.new
+    end
+
+    def create
+        @event = current_user.created_events.build(event_params)
+        if @event.save
+            redirect_to @event
+        else
+            render 'new'
+        end
+    end
+
+    def show
+        @event = Event.find(params[:id])
+    end
+    
+    private
+        def event_params
+            params.require(:event).permit(:description)
+        end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150914194154) do
+ActiveRecord::Schema.define(version: 20150920035101) do
+
+  create_table "events", force: :cascade do |t|
+    t.text     "description"
+    t.datetime "date"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "creator_id"
+  end
+
+  add_index "events", ["creator_id"], name: "index_events_on_creator_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
